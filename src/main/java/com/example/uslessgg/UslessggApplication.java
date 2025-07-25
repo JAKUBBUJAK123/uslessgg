0ccb2b8c911043e226e0d125e671ad39d0502192
Jakub Bujak <jakbuj3@gmail.com>
2025-07-25 11:50:34 +0200

    initial commit

---
+++ src/main/java/com/example/uslessgg/UslessggApplication.java
@@ -0,0 +1,14 @@
+package com.example.uslessgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UslessggApplication {
+
+    public static void main(String[] args) {
+        System.out.println("im working");
+        SpringApplication.run(UslessggApplication.class, args);
+    }
+
+}
